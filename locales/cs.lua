8b082c41cefc27f5b58fb72bcfcb41ff6cf2404e
DexikCZ123 <83772359+DexikCZ123@users.noreply.github.com>
2022-03-09 22:51:04 +0100

    Add files via upload

---
+++ locales/cs.lua
@@ -0,0 +1,6 @@
+Locales['cs'] = {
+    ['press_e'] = '~INPUT_CONTEXT~ for laundry money',
+    ['dont_have_blackmoney'] = 'You dont have black money!',
+    ['laundering_money'] = 'Laundering Money...',
+
+}
---
+++ locales/en.lua
@@ -0,0 +1,6 @@
+Locales['en'] = {
+    ['press_e'] = '~INPUT_CONTEXT~ pro prani penez',
+    ['dont_have_blackmoney'] = 'Nemate dostatek penez!',
+    ['laundering_money'] = 'Prani penez...',
+
+}
